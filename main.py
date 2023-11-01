1bbcaf120a345e59e65f831342c151ddbe9f8367
pronuld7 <Pron_uld7@student.itstep.org>
2023-11-01 18:17:32 -0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# ATTENTION! THIS MIGHT NOT WORK ON WINDOWS, AS I TRIED TO IMPORT THE MODULE THAT
+# FIXES IT, BUT IT DOES NOT EXIST
+import colorama # Import the library
+from colorama import Fore, Back, Style # Import 3 modules from the library
+print(Fore.GREEN + 'Hello!') # Print green text
+print(Back.BLACK + 'Teacher') # Print green text with black background
+print(Style.BRIGHT + 'IT Step') # Print bold/bright text with black background
